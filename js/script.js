130ea9bcb6e20c07acebe5209d54a42d97acbaa5
unknown <annahrishko@ukr.net>
2023-05-05 12:42:17 +0300

    add career.html, single-career.html

--- js/script.js
+++ js/script.js
@@ -1,6 +1,3 @@
-
-
-
 function customHeight() {
    if ($(window).width() > 991) {
      $('.wraper_for_custom_height').each(function(){  
@@ -21,4 +18,39 @@ customHeight()
 $(window).resize(function() {
   customHeight()
   console.log("hello")
+});
+
+$(function() {
+    $.validator.addMethod("emailRegex", function(value, element) {
+        return this.optional(element) || /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/i.test(value);
+    }, "");
+});
+
+$(function() {
+    $(".career_form").validate({
+        rules: {
+            "email": {
+                required: true,
+                emailRegex: true,
+            },
+            "name": {
+                required: true,
+            },
+            "link_cv": {
+                required: true,
+            },
+        },
+        messages: {
+            "email": {
+                required: "Required feild",
+                emailRegex: "Enter valid email address",
+            },
+            "name": {
+                required: "Required feild",
+            },
+            "link_cv": {
+                required: "Required feild",
+            },
+        },
+    });
 });
